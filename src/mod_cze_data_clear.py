c9e5045cbb0bbe77cf72cf78aabb06c4d35451bc
jihuaizhi <jhzbs@sina.com>
2019-03-17 01:08:47 +0800

    1

---
+++ src/mod_cze_data_clear.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# python 3.7.2
+# @Time    : 2019/3/16
+# @Author  : jihuaizhi
+# @Desc    : CZE平台历史数据清理程序,依据数据库中的记录,清理课件,CTF,AWD,靶场的文本类文件,视频类文件
+
+
+
+# 模块调试
+if __name__ == '__main__':
+    pass
